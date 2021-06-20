c6e5e46466c6bc4d27cd377b065bf765c1a1ab50
Tsuyoshi Kodama <tsuyoshi.kodama@byrnison.com>
2021-06-21 08:44:50 +0900

    chore: commented out importing panfluet to be executable without panfluet and any other pip modules.

--- src/app/dump/command.py
+++ src/app/dump/command.py
@@ -2,7 +2,7 @@
 command.py
 """
 import sys
-import panflute as pf
+# import panflute as pf
 
 __subcommand__ = 'dump'
 
@@ -31,14 +31,14 @@ def run(args):
 
         with open(args.dumpfile, 'r', encoding='UTF-8') as f:
             dumpfile = f.read(-1)
-            dumpfile = pf.convert_text(dumpfile, input_format=format, output_format='html')
-            doc = pf.convert_text(dumpfile, input_format='html', standalone=True)
+    #         dumpfile = pf.convert_text(dumpfile, input_format=format, output_format='html')
+    #         doc = pf.convert_text(dumpfile, input_format='html', standalone=True)
 
-    elif not sys.stdin.isatty():
-        # file名の指定がない場合は、パイプから読み込みます。
-        # 標準入力が tty ではないことを確認して、標準入力をパイプと判定しています。
-        # pandoc -f gfm+sourcepos -t html ../ghost/process/README.md | pandoc -f html -t json | gdoc dump
-        doc = pf.load()
+    # elif not sys.stdin.isatty():
+    #     # file名の指定がない場合は、パイプから読み込みます。
+    #     # 標準入力が tty ではないことを確認して、標準入力をパイプと判定しています。
+    #     # pandoc -f gfm+sourcepos -t html ../ghost/process/README.md | pandoc -f html -t json | gdoc dump
+    #     doc = pf.load()
 
     else:
         print(__subcommand__ + ': error: Missing dumpfile ( [-d / --dumpfile] is required)')
@@ -72,18 +72,18 @@ def stringify(elem):
 
     text = ''
 
-    if hasattr(elem, 'content'):
-        text = ''
-    elif isinstance(elem, pf.Space):
-        text = '<space>'
-    elif isinstance(elem, pf.SoftBreak):
-        text = '<sb>\n'
-    elif isinstance(elem, pf.LineBreak):
-        text = '<lb>\n'
-    elif isinstance(elem, pf.HorizontalRule):
-        text = '\n--- HorizontalRule ---\n'
-    else:
-        text = pf.stringify(elem)
+    # if hasattr(elem, 'content'):
+    #     text = ''
+    # elif isinstance(elem, pf.Space):
+    #     text = '<space>'
+    # elif isinstance(elem, pf.SoftBreak):
+    #     text = '<sb>\n'
+    # elif isinstance(elem, pf.LineBreak):
+    #     text = '<lb>\n'
+    # elif isinstance(elem, pf.HorizontalRule):
+    #     text = '\n--- HorizontalRule ---\n'
+    # else:
+    #     text = pf.stringify(elem)
 
     return text
 
@@ -93,35 +93,35 @@ def print_element(elem, prestr):
     else:
         id = prestr
 
-    print('\n>> ' + elem.__class__.__name__ + ' [' + id + ']')
+    # print('\n>> ' + elem.__class__.__name__ + ' [' + id + ']')
 
-    if isinstance(elem, pf.Str):
-        print('(Str element) --> ' + pf.stringify(elem))
-    elif isinstance(elem, pf.Block):
-        print('(Block element)')
-    elif isinstance(elem, pf.Inline):
-        print('(Inline element)')
-    elif isinstance(elem, pf.MetaValue):
-        print('(MetaValue element)')
-    else:
-        print('(unknown category element)')
+    # if isinstance(elem, pf.Str):
+    #     print('(Str element) --> ' + pf.stringify(elem))
+    # elif isinstance(elem, pf.Block):
+    #     print('(Block element)')
+    # elif isinstance(elem, pf.Inline):
+    #     print('(Inline element)')
+    # elif isinstance(elem, pf.MetaValue):
+    #     print('(MetaValue element)')
+    # else:
+    #     print('(unknown category element)')
 
-    if elem.location is not None:
-        print('location = ' + elem.location)
+    # if elem.location is not None:
+    #     print('location = ' + elem.location)
 
-    if hasattr(elem, 'attributes'):
-        print('attributes length = ', len(elem.attributes))
-        print('attributes = ', elem.attributes)
-        if 'pos' in elem.attributes:
-            print('source pos = ', elem.attributes['pos'])
+    # if hasattr(elem, 'attributes'):
+    #     print('attributes length = ', len(elem.attributes))
+    #     print('attributes = ', elem.attributes)
+    #     if 'pos' in elem.attributes:
+    #         print('source pos = ', elem.attributes['pos'])
 
-    if hasattr(elem, 'content'):
-        print('content length = ', len(elem.content))
+    # if hasattr(elem, 'content'):
+    #     print('content length = ', len(elem.content))
 
-    if elem.index is not None:
-        print('index = ', elem.index)
+    # if elem.index is not None:
+    #     print('index = ', elem.index)
 
-    if hasattr(elem, 'container'):
-        print('container type = ', elem.container.__class__.__name__)
+    # if hasattr(elem, 'container'):
+    #     print('container type = ', elem.container.__class__.__name__)
 
-    print('Element Stringify >> ' + pf.stringify(elem))
+    # print('Element Stringify >> ' + pf.stringify(elem))
