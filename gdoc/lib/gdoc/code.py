7b4cee0bd60e2569338610dcd4bb1a65f4092586
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2022-10-11 03:05:05 +0000

    refact: changed 'Code.__init__' to accept str.

--- gdoc/lib/gdoc/code.py
+++ gdoc/lib/gdoc/code.py
@@ -2,6 +2,9 @@
 code.py: `Code` inline element class
 """
 
+from typing import cast
+
+from gdoc.lib.pandocastobject.pandocast import PandocAst
 from gdoc.lib.pandocastobject.pandocast.element import Element
 
 from .text import Text
@@ -10,7 +13,16 @@ from .text import Text
 class Code(Text):
     """ """
 
-    def __init__(self, element: Element):
+    def __init__(self, item: Element | str):
+        element: Element
+        if type(item) is str:
+            element = PandocAst.create_element({"t": "Code", "c": [["", [], []], item]})
+        else:
+            element = cast(Element, item)
+
+        if element.get_type() != "Code":
+            raise RuntimeError()
+
         self.element = element
 
     def get_str(self):
