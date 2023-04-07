6f1faa53c0675b021f63a46d0af86ed201f27bf4
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2023-04-07 13:30:24 +0000

    test: Cleaned tests up.

--- spec/lib/pandocastobject/pandocast/spec_types.py
+++ spec/lib/pandocastobject/pandocast/spec_types.py
@@ -18,160 +18,127 @@ The specification of Element class.
 | @Method  | PandocAst      | Creates a PandocAst object and returns it.
 
 """
+import inspect
+
 import pytest
 
-from gdoc.lib.pandocastobject import pandocast
-from gdoc.lib.pandocastobject.pandocast import types
-from gdoc.lib.pandocastobject.pandocast.pandoc import Pandoc
-
-## @{ @name create_element(pan_elem, elem_type=None)
-## [\@spec create_element] Find the element type and call constructor specified by it.
-_create_element = "dummy for doxygen styling"
+from gdoc.lib.pandocastobject.pandocast import PandocAst
 
 
-def spec_create_element_1(mocker):
+class Spec_create_element:
     r"""
-    [\@Spec create_element.1] 't'
+    ## [\@spec] `create_element`
+
+    ```py
+    def create_element(pan_elem, elem_type=None) -> Element:
+    ```
     """
-    Class_mock = mocker.Mock(return_value="INSTANCE")
-    elem_types = {"TARGET": {"class": Class_mock}}
-    mocker.patch("gdoc.lib.pandocastobject.pandocast.types._ELEMENT_TYPES", elem_types)
 
-    pan_elem = {"t": "TARGET"}
-    element = types.PandocAst.create_element(pan_elem)
+    @staticmethod
+    def cases_1():
+        r"""
+        ### [\@ 1]
+        """
+        return {
+            ##
+            # #### [\@case 1] Normal
+            #
+            "Simple(1/)": (
+                # precondition
+                {"ELEMENT_TYPE": "TEST_TYPE"},
+                # stimulus
+                {"pan_elem": {"t": "TEST_TYPE"}},
+                # expected
+                {
+                    "Exception": None,
+                    "element_type": "TEST_TYPE",
+                },
+            ),
+            "Simple(2/)": (
+                # precondition
+                {"ELEMENT_TYPE": "Pandoc"},
+                # stimulus
+                {"pan_elem": {"pandoc-api-version": [1, 22]}},
+                # expected
+                {
+                    "Exception": None,
+                    "element_type": "Pandoc",
+                },
+            ),
+            "Simple(3/)": (
+                # precondition
+                {"ELEMENT_TYPE": "TEST_TYPE"},
+                # stimulus
+                {"pan_elem": [], "elem_type": "TEST_TYPE"},
+                # expected
+                {
+                    "Exception": None,
+                    "element_type": "TEST_TYPE",
+                },
+            ),
+            ##
+            # #### [\@case 2] Error
+            #
+            "Simple(4/)": (
+                # precondition
+                {"ELEMENT_TYPE": "TEST_TYPE"},
+                # stimulus
+                {"pan_elem": []},
+                # expected
+                {
+                    "Exception": [KeyError, "ELEMENT TYPE MISSING"],
+                },
+            ),
+            "Simple(5/)": (
+                # precondition
+                {"ELEMENT_TYPE": "TEST_TYPE"},
+                # stimulus
+                {"pan_elem": [], "elem_type": "TEST_INVALID_ELEMENT_TYPE"},
+                # expected
+                {
+                    "Exception": [KeyError, "TEST_INVALID_ELEMENT_TYPE"],
+                },
+            ),
+        }
 
-    assert element == "INSTANCE"
+    # \cond
+    @pytest.mark.parametrize(
+        "precondition, stimulus, expected",
+        list(cases_1().values()),
+        ids=list(cases_1().keys()),
+    )
+    # \endcond
+    def spec_1(self, mocker, precondition, stimulus, expected):
+        r"""
+        ### [\@spec 1]
+        """
+        # GIVEN
+        Class_mock = mocker.Mock(return_value="INSTANCE")
+        mocker.patch.object(
+            inspect.getmodule(PandocAst),
+            "_ELEMENT_TYPES",
+            {precondition["ELEMENT_TYPE"]: {"class": Class_mock}},
+        )
 
-    args = Class_mock.call_args_list
+        if expected["Exception"] is None:
+            # WHEN
+            element = PandocAst.create_element(**stimulus)
+            # THEN
+            assert element == "INSTANCE"
+            assert Class_mock.call_count == 1
+            assert Class_mock.call_args_list[0] == [
+                (
+                    stimulus["pan_elem"],
+                    expected["element_type"],
+                    {"class": Class_mock},
+                    PandocAst.create_element,
+                ),
+                {},
+            ]
 
-    assert Class_mock.call_count == 1
-    assert args[0] == [
-        (pan_elem, "TARGET", elem_types["TARGET"], types.PandocAst.create_element),
-        {},
-    ]
-
-
-def spec_create_element_2(mocker):
-    r"""
-    [\@Spec create_element.2] Pandoc
-    """
-    Class_mock = mocker.Mock(return_value="INSTANCE")
-    elem_types = {"Pandoc": {"class": Class_mock}}
-    mocker.patch("gdoc.lib.pandocastobject.pandocast.types._ELEMENT_TYPES", elem_types)
-
-    pan_elem = {"pandoc-api-version": [1, 22]}
-    element = types.PandocAst.create_element(pan_elem)
-
-    assert element == "INSTANCE"
-
-    args = Class_mock.call_args_list
-
-    assert Class_mock.call_count == 1
-    assert args[0] == [
-        (pan_elem, "Pandoc", elem_types["Pandoc"], types.PandocAst.create_element),
-        {},
-    ]
-
-
-def spec_create_element_3(mocker):
-    r"""
-    [\@Spec create_element.3] elem_type
-    """
-    Class_mock = mocker.Mock(return_value="INSTANCE")
-    elem_types = {"TARGET": {"class": Class_mock}}
-    mocker.patch("gdoc.lib.pandocastobject.pandocast.types._ELEMENT_TYPES", elem_types)
-
-    pan_elem = []
-    element = types.PandocAst.create_element(pan_elem, "TARGET")
-
-    assert element == "INSTANCE"
-
-    args = Class_mock.call_args_list
-
-    assert Class_mock.call_count == 1
-    assert args[0] == [
-        (pan_elem, "TARGET", elem_types["TARGET"], types.PandocAst.create_element),
-        {},
-    ]
-
-
-def spec_create_element_4(mocker):
-    r"""
-    [\@Spec create_element.4] Missing element type -> should raise
-    """
-    Class_mock = mocker.Mock(return_value="INSTANCE")
-    elem_types = {"TARGET": {"class": Class_mock}}
-    mocker.patch("gdoc.lib.pandocastobject.pandocast.types._ELEMENT_TYPES", elem_types)
-
-    pan_elem = []
-
-    with pytest.raises(Exception) as e:
-        element = types.PandocAst.create_element(pan_elem)
-
-    assert str(e.value) == "'ELEMENT TYPE MISSING'"
-
-
-def spec_create_element_5(mocker):
-    r"""
-    [\@Spec create_element.4] Invalid element type -> should raise
-    """
-    Class_mock = mocker.Mock(return_value="INSTANCE")
-    elem_types = {"TARGET": {"class": Class_mock}}
-    mocker.patch("gdoc.lib.pandocastobject.pandocast.types._ELEMENT_TYPES", elem_types)
-
-    pan_elem = []
-
-    with pytest.raises(Exception) as e:
-        element = types.PandocAst.create_element(pan_elem, "TEST_INVALID_ELEMENT_TYPE")
-
-    assert str(e.value) == "'TEST_INVALID_ELEMENT_TYPE'"
-
-
-## @}
-## @{ @name PandocAst(pan_elem)
-## [\@spec PandocAst] Creates a PandocAst object and returns it.
-_PandocAst = "dummy for doxygen styling"
-
-
-def spec_PandocAst_1():
-    r"""
-    [\@Spec PandocAst.1] alias
-    """
-    assert pandocast.PandocAst is types.PandocAst
-
-
-## @}
-## @{ @name \_\_init\_\_(pan_elem)
-## [\@spec \_\_init\_\_] creates a new PandocAst instance.
-##
-___init__ = "dummy for doxygen styling"
-
-
-def spec___init___1():
-    r"""
-    [@spec \_\_init\_\_.1] set props with default values.
-    """
-    _ELEMENT = {"blocks": []}
-    _TYPE_DEF = {
-        "Pandoc": {
-            # Pandoc Meta [Block]
-            "class": Pandoc,
-            "content": {"key": None, "main": "blocks", "type": "[Block]"},
-            "struct": {
-                "Version": "pandoc-api-version",
-                "Meta": "meta",
-                "Blocks": "blocks",
-            },
-        },
-    }
-
-    target = Pandoc(_ELEMENT, "Pandoc", _TYPE_DEF["Pandoc"], "dummy_function")
-
-    assert target.pan_element is _ELEMENT
-    assert target.type == "Pandoc"
-    assert target.type_def is _TYPE_DEF["Pandoc"]
-    assert target.parent is None
-    assert target.children == []
-
-
-## @}
+        else:
+            # WHEN
+            with pytest.raises(expected["Exception"][0]) as exc_info:
+                PandocAst.create_element(**stimulus)
+            # THEN
+            assert exc_info.match(expected["Exception"][1])
