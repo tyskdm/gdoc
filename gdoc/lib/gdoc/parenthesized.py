aef2e94b60ac7469be5341f37eb7a1623f2e0be6
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2023-05-14 11:10:14 +0000

    feat: Added 'loadd()' and 'dumpd()' methods to 'Parenthesized' class.

--- gdoc/lib/gdoc/parenthesized.py
+++ gdoc/lib/gdoc/parenthesized.py
@@ -8,3 +8,18 @@ class Parenthesized(TextString, ret_subclass=False):
     """
     Parenthesized TextString class
     """
+
+    def dumpd(self) -> list:
+        textstr_dumpdata: list = super().dumpd()
+        textstr_dumpdata[0] = "P"
+        return textstr_dumpdata
+
+    @classmethod
+    def loadd(cls, data: list) -> "Parenthesized":
+
+        if data[0] != "P":
+            raise TypeError("invalid data type")
+
+        textstr: TextString = TextString.loadd(["T"] + data[1:])
+
+        return cls(textstr)
--- spec/lib/gdoc/spec_Parenthesised.py
+++ spec/lib/gdoc/spec_Parenthesised.py
@@ -6,6 +6,7 @@ r"""
 ## ADDITIONAL STRUCTURE
 
 """
+import pytest
 from gdoc.lib.gdoc.parenthesized import Parenthesized, TextString
 
 
@@ -28,3 +29,84 @@ class Spec_Parenthesized:
         # THEN
         assert isinstance(target, TextString)
         assert isinstance(target, Parenthesized)
+
+
+class Spec_loadd_and_dumpd:
+    r"""
+    ## [\@spec] `loadd` and `dumpd`
+
+    ```py
+    def loadd(cls, data: list) -> "Parenthesized":
+    def dumpd(self) -> list:
+    ```
+    """
+
+    @staticmethod
+    def cases_1():
+        r"""
+        ### [\@ 1]
+        """
+        return {
+            ##
+            # #### [\@case 1] Normal:
+            #
+            "Normal(1/)": (
+                # stimulus
+                ["P", [["s", "(ABC)"]]],
+                # expected
+                {
+                    "Exception": None,
+                },
+            ),
+            "Normal(2/)": (
+                # stimulus
+                ["P", [["s", "[AB"], ["c", '"'], ["s", "CD]"]]],
+                # expected
+                {
+                    "Exception": None,
+                },
+            ),
+            ##
+            # #### [\@case 2] Error:
+            #
+            "Error(1/)": (
+                # stimulus
+                ["T", [["s", "(ABC)"]]],
+                # expected
+                {
+                    "Exception": [
+                        TypeError,
+                        "invalid data type",
+                    ],
+                },
+            ),
+        }
+
+    # \cond
+    @pytest.mark.parametrize(
+        "stimulus, expected",
+        list(cases_1().values()),
+        ids=list(cases_1().keys()),
+    )
+    # \endcond
+    def spec_1(self, stimulus, expected):
+        r"""
+        ### [\@spec 1]
+        """
+        if expected["Exception"] is None:
+            # WHEN
+            quoted = Parenthesized.loadd(stimulus)
+            dumpdata = quoted.dumpd()
+
+            # THEN
+            assert dumpdata is not stimulus
+            assert dumpdata == stimulus
+
+        else:
+            # WHEN
+            with pytest.raises(expected["Exception"][0]) as exc_info:
+                quoted = Parenthesized.loadd(stimulus)
+                dumpdata = quoted.dumpd()
+
+            # THEN
+            assert exc_info.match(expected["Exception"][1])
