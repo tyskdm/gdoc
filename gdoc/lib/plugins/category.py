e9b2e8c6ec863c6a260a1495d83a73c4a9016038
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2023-07-25 07:00:38 +0000

    refactor: Changed so that aliases don't override defaults.

--- gdoc/lib/plugins/category.py
+++ gdoc/lib/plugins/category.py
@@ -27,13 +27,15 @@ class Category:
         # Get default type if target_type is not provided
         if target_type in ("", None):
             if parent_type in self.defaults:
-                target_type = self.defaults[parent_type]
+                type_name = self.defaults[parent_type]
 
-        if target_type not in ("", None):
+        else:
             type_name = target_type.upper()
-
             if type_name in aliases:
                 type_name = aliases[type_name]
+
+        if type_name not in ("", None):
+            # type_name = target_type.upper()
 
             if type_name in self.aliases:
                 type_name = self.aliases[type_name]
