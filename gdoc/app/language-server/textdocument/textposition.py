d57085870fb66ffc4805accd34b4a9e46dfc2e60
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2023-09-04 04:56:07 +0000

    refactor: Changed 'Line' to @dataclass from TypedDict.

--- gdoc/app/language-server/textdocument/textposition.py
+++ gdoc/app/language-server/textdocument/textposition.py
@@ -1,12 +1,14 @@
+from dataclasses import dataclass
 from logging import getLogger
-from typing import TypedDict, cast
+from typing import cast
 
 logger = getLogger(__name__)
 
 UTF16_TYPE: str = "utf-16-le"
 
 
-class Line(TypedDict):
+@dataclass
+class Line:
     text: str
     utf16: bytes | None  # bytes(utf-16-le)
 
@@ -41,7 +43,7 @@ class TextPosition:
         if column >= length[1]:
             return length[0]
 
-        u16_line: bytes = cast(bytes, self.lines[line]["utf16"])
+        u16_line: bytes = cast(bytes, self.lines[line].utf16)
         return len(u16_line[: column * 2].decode(UTF16_TYPE))
 
     def get_u16_column(self, line: int, column: int) -> int:
@@ -60,7 +62,7 @@ class TextPosition:
         offset_char: int = 0
         offset_utf16: int = 0
         i: int = -1
-        for i, offset in enumerate(self.lines[line]["offsets"]):
+        for i, offset in enumerate(self.lines[line].offsets):
             if type(offset) is int:
                 # len(utf-8) == len(utf-16-le)
                 if chars > offset:
@@ -113,40 +115,40 @@ class TextPosition:
         return cast(tuple[int, int], length)[1]
 
     def _get_length(self, line: int) -> int | tuple[int, int]:
-        length: None | int | tuple[int, int] = self.lines[line]["length"]
+        length: None | int | tuple[int, int] = self.lines[line].length
         if length is None:
             # The line has not been checked yet.
             length = self._set_linedata(line)
         return length
 
-    def _set_linedata(self, line) -> int | tuple[int, int]:
-        text: str = self.lines[line]["text"]
+    def _set_linedata(self, line: int) -> int | tuple[int, int]:
+        text: str = self.lines[line].text
         text16: bytes = text.encode("utf_16_le")
         textlen: int = len(text)
         text16len: int = len(text16) >> 1  # len(text16) / 2
 
         if textlen != text16len:
             length = (textlen, text16len)
-            self.lines[line]["length"] = length
-            self.lines[line]["utf16"] = text16
+            self.lines[line].length = length
+            self.lines[line].utf16 = text16
         else:
             length = textlen
-            self.lines[line]["length"] = textlen
+            self.lines[line].length = textlen
 
         return length
 
     def _get_u16_offset(self, line: int, index: int, start: int, chars: int) -> int:
         chars_utf16: int = (
-            len(self.lines[line]["text"][start : start + chars].encode(UTF16_TYPE))
+            len(self.lines[line].text[start : start + chars].encode(UTF16_TYPE))
             >> 1  # len(utf-16-le) / 2
         )
 
         new_offset: int | tuple[int, int] = (
             (chars, chars_utf16) if chars != chars_utf16 else chars
         )
-        if index < len(self.lines[line]["offsets"]):
+        if index < len(self.lines[line].offsets):
             offset: tuple[int, int] = cast(
-                tuple[int, int], self.lines[line]["offsets"][index]
+                tuple[int, int], self.lines[line].offsets[index]
             )
             old_offset: int | tuple[int, int] = (
                 offset[0] - chars,
@@ -155,8 +157,8 @@ class TextPosition:
             if old_offset[0] == old_offset[1]:
                 old_offset = offset[0]
 
-            self.lines[line]["offsets"][index : index + 1] = [new_offset, offset]
+            self.lines[line].offsets[index : index + 1] = [new_offset, offset]
         else:
-            self.lines[line]["offsets"].append(new_offset)
+            self.lines[line].offsets.append(new_offset)
 
         return chars_utf16
