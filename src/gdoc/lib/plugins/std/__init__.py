fe93cd9d6d8b45628c15a6d54c04276a216d8fcd
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2025-08-22 11:14:16 +0000

    fix: Correct typo in SysML types definition from "Bloooock" to "Block"

--- src/gdoc/lib/plugins/std/__init__.py
+++ src/gdoc/lib/plugins/std/__init__.py
@@ -29,7 +29,7 @@ category = Category(
             #
             # SysML types
             #
-            "Bloooock": type(
+            "Block": type(
                 "std:Block",
                 (Object, Property, Trait),
                 {
